aa9e9c83d76f4d86b83a4ce9331d90944bdcc623
Prashant <prashant220405@gmail.com>
2026-07-25 22:04:21 +0530

    Implement minimum average calculation for array elements

---
+++ 3194. Minimum Average of Smallest and Largest Elements/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double minimumAverage(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int i = 0;
+        int j = nums.length - 1;
+
+        double ans = Double.MAX_VALUE;
+
+        while (i < j) {
+
+            double avg = (nums[i] + nums[j]) / 2.0;
+            ans = Math.min(ans, avg);
+
+            i++;
+            j--;
+        }
+
+        return ans;
+    }
+}
