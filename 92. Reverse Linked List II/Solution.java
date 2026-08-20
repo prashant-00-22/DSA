da60b8206ad07ade9820554e5e1133358fc8cfb4
Prashant <prashant220405@gmail.com>
2026-08-20 22:34:58 +0530

    Implement reverseBetween method for linked list

---
+++ 92. Reverse Linked List II/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        if (head == null || left == right) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        // left se pehle wale node tak jao
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+
+        // left se right tak reverse
+        for (int i = 0; i < right - left; i++) {
+            ListNode temp = curr.next;
+
+            curr.next = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;
+        }
+
+        return dummy.next;
+    }
+}
